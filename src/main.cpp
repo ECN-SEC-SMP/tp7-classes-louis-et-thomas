74387bd43de5800b3a186f818d9abd27bdc6d566
Thomas Verron <verronthomas03@gmail.com>
2025-01-31 09:24:35 +0100

    Main test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "Point.h"
 
-int main(int argc, char const *argv[])
+
+int main()
 {
-    std::cout << "Hello World !" << std::endl;
-    return 0;
+    Point p = Point(5,6);
+    std::cout << "La valeur de x est " << p.getX() << std::endl;
+    std::cout << "La valeur de y est " << p.getY() << std::endl;
+    p.translater(12,3);
+    std::cout << "Apres translation la valeur de x est " << p.getX() << std::endl;
+    std::cout << "Apres translation la valeur de y est " << p.getY() << std::endl;
 }
